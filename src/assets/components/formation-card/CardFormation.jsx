494e17ce9fc3695dc8bae068346c93018b257022
MrAbelDev <abelmruso@gmail.com>
2025-10-02 17:32:25 +0200

    mise en page des cards stack technologique

--- src/assets/components/formation-card/CardFormation.jsx
+++ src/assets/components/formation-card/CardFormation.jsx
@@ -2,14 +2,14 @@ import styles from "../formation-card/CardFormation.module.scss";
 
 function CardFormation({ title, subtitle, date, children, layout = "side" }) {
     return (
-        <article className={`card ${layout}`}>
+        <article className={`${styles.card} ${styles[layout]}`}>
             <div className={styles.contenair}>
-                <h3>{title}</h3>
+                <h3 className={styles.title}>{title}</h3>
                 {subtitle && <p>{subtitle}</p>}
                 {date && <span>{date}</span>}
             </div>
 
-            {children && <div>{children}</div>}
+            {children && <div className={styles.iconsContenair}>{children}</div>}
         </article>
     );
 }

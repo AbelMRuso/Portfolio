13d9bbfdf98464c968cc5e0ffcb95db8c057908d
MrAbelDev <abelmruso@gmail.com>
2025-10-02 12:23:10 +0200

    mise en forme du composant Hero

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,8 +1,15 @@
+import profil from "../images/profil_image.png";
+import styles from "../styles/Hero.module.scss";
+
 function Hero() {
     return (
-        <section>
-            <h1>Título de prueba</h1>;<h4>Subtítulo de prueba</h4>
-            <p>Texto de prueba</p>
+        <section className={styles.contenair}>
+            <img src={profil} alt="Image de profil" />
+            <div>
+                <h1>Abel Martínez Ruso</h1>
+                <h2>Développeur full stack</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem non beatae voluptatem illum ipsum</p>
+            </div>
         </section>
     );
 }

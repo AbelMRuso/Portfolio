6069f588eb385b2ec1defb18c78bb2837ab63c4c
MrAbelDev <abelmruso@gmail.com>
2025-10-03 16:02:30 +0200

    adition props à ProjectCard

--- src/assets/components/project-card/ProjectCard.jsx
+++ src/assets/components/project-card/ProjectCard.jsx
@@ -1,28 +1,27 @@
 import styles from "./ProjectCard.module.scss";
-import imageTest from "../../images/image_test.png";
-import imageTest2 from "../../images/image_test2.png";
-import imageTest3 from "../../images/image_test3.png";
-import imageTest4 from "../../images/image_test4.png";
+import { FaSearch } from "react-icons/fa";
 
-function ProjectCard() {
+function ProjectCard({ mainImg, thumbnails, description, technologies, date }) {
     return (
         <>
-            <h2>Hola mundo</h2>
             <article className={styles.card}>
-                <img className={styles.mainImg} src={imageTest} alt="" />
+                <img className={styles.mainImg} src={mainImg} alt="" />
                 <div className={styles.cardContent}>
                     <div className={styles.thumbnails}>
-                        <img className={styles.testImg} src={imageTest2} alt="" />
-                        <img className={styles.testImg} src={imageTest3} alt="" />
-                        <img className={styles.testImg} src={imageTest4} alt="" />
+                        <div>
+                            <img className={styles.testImg} src={thumbnails} alt="" />
+                            <div>
+                                <FaSearch className={styles.search} />
+                            </div>
+                        </div>
                     </div>
-                    <p className={styles.text}>Site web de photographie complet avec paneau de bord admin, login, et formulaires</p>
+
+                    <p className={styles.text}>{description}</p>
                     <div className={styles.tagContenair}>
-                        <span className={styles.tag}>React</span>
-                        <span className={styles.tag}>Node</span>
+                        <span className={styles.tag}>{technologies}</span>
                     </div>
                     <div className={styles.infoContenair}>
-                        <p>Avril 2025</p>
+                        <p>{date}</p>
                         <a>+ d'Info</a>
                     </div>
                 </div>

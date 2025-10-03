a62b7e9cf9d6d6ae4d3376ad33c944f0e4150192
MrAbelDev <abelmruso@gmail.com>
2025-10-03 12:23:57 +0200

    test images miniature

--- src/assets/components/project-card/ProjectCard.jsx
+++ src/assets/components/project-card/ProjectCard.jsx
@@ -1,5 +1,8 @@
 import styles from "./ProjectCard.module.scss";
 import imageTest from "../../images/image_test.png";
+import imageTest2 from "../../images/image_test2.png";
+import imageTest3 from "../../images/image_test3.png";
+import imageTest4 from "../../images/image_test4.png";
 
 function ProjectCard() {
     return (
@@ -9,9 +12,9 @@ function ProjectCard() {
                 <img className={styles.mainImg} src={imageTest} alt="" />
                 <div className={styles.cardContent}>
                     <div className={styles.thumbnails}>
-                        <img className={styles.testImg} src="" alt="" />
-                        <img className={styles.testImg} src="" alt="" />
-                        <img className={styles.testImg} src="" alt="" />
+                        <img className={styles.testImg} src={imageTest2} alt="" />
+                        <img className={styles.testImg} src={imageTest3} alt="" />
+                        <img className={styles.testImg} src={imageTest4} alt="" />
                     </div>
                     <p className={styles.text}>Site web de photographie complet avec paneau de bord admin, login, et formulaires</p>
                     <div className={styles.tagContenair}>

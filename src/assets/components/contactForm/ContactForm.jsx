ff19267b1881bc6374868a6ffa6b372ef23d38e7
MrAbelDev <abelmruso@gmail.com>
2025-10-06 15:32:20 +0200

    configuration emailjs

--- src/assets/components/contactForm/ContactForm.jsx
+++ src/assets/components/contactForm/ContactForm.jsx
@@ -11,10 +11,10 @@ export default function ContactForm() {
 
         emailjs
             .sendForm(
-                "TU_SERVICE_ID", // tu Service ID
-                "TU_TEMPLATE_ID", // tu Template ID
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
                 form.current,
-                "TU_PUBLIC_KEY" // tu Public Key
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
             )
             .then(
                 () => {

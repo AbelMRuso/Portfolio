f5d2475ab5f28b0f7943447faaf227d71ea5b29b
MrAbelDev <abelmruso@gmail.com>
2025-10-08 17:07:36 +0200

    Version responsive mobile et tablette mise en place

--- src/assets/components/about-section/AboutSection.jsx
+++ src/assets/components/about-section/AboutSection.jsx
@@ -1,7 +1,7 @@
 import AboutBlock from "../about-block/AboutBlock";
 import styles from "../about-section/AboutSection.module.scss";
 import imgContext from "../../../../public/images/img-contexte.jpeg";
-import imgParcours from "../../../../public/images/img-parcours2.jpg";
+import imgParcours from "../../../../public/images/img-parcours.jpg";
 import imgStack from "../../../../public/images/img-stack2.jpg";
 import imgAmbitions from "../../../../public/images/img-ambitions.jpg";
 
@@ -14,14 +14,25 @@ function AboutSection() {
                     image={imgContext}
                     title={"Contexte"}
                     text={
-                        "Toujours en quête de nouveaux défis, je me suis installé sur la côte Atlantique pour apprendre une nouvelle langue et vivre de nouvelles expériences, tout en poursuivant ma passion pour le surf. Mon intérêt pour la programmation est né lorsque, travaillant comme coordinateur logistique, un bug m’a amené à collaborer avec des développeurs : ce moment a déclenché une véritable étincelle et a éveillé ma curiosité pour le code."
+                        <>
+                            Toujours en quête de nouveaux défis, je me suis installé sur la côte Atlantique pour apprendre une nouvelle langue et
+                            vivre de nouvelles expériences, tout en poursuivant ma passion pour le surf.<br></br>
+                            <br></br> Mon intérêt pour la programmation est né lorsque, travaillant comme coordinateur logistique, un bug m’a amené à
+                            collaborer avec des développeurs : ce moment a déclenché une véritable étincelle et a éveillé ma curiosité pour le code.
+                        </>
                     }
                 />
                 <AboutBlock
                     image={imgParcours}
                     title={"Parcours profesionnels"}
                     text={
-                        "Mon parcours a toujours été guidé par le sport et l’enseignement. J’ai passé sept ans à encadrer des disciplines nautiques, coordonner des équipes et gérer un centre, tout en restant en contact avec la nature et les personnes. Ma formation en éducation primaire m’a apporté des compétences pédagogiques, psychologiques et méthodologiques solides. Aujourd’hui, je mets ces acquis et cette expérience au service de ma reconversion vers le développement web, un domaine où je peux allier rigueur, curiosité et goût pour les défis."
+                        <>
+                            Mon parcours a toujours été guidé par le sport et l’enseignement. J’ai passé sept ans à encadrer des disciplines
+                            nautiques, coordonner des équipes et gérer un centre, tout en restant en contact avec la nature et les personnes.<br></br>
+                            <br></br> Ma formation en éducation primaire m’a apporté des compétences pédagogiques, psychologiques et méthodologiques
+                            solides. Aujourd’hui, je mets ces acquis et cette expérience au service de ma reconversion vers le développement web, un
+                            domaine où je peux allier rigueur, curiosité et goût pour les défis."
+                        </>
                     }
                     reverse
                 />
@@ -29,14 +40,24 @@ function AboutSection() {
                     image={imgStack}
                     title={"Methodologie et stack"}
                     text={
-                        "Ayant principalement travaillé en solo, j’ai appris à apprécier l’organisation et le suivi rigoureux des projets, notamment grâce aux méthodes agiles et à l’utilisation d’un tableau Kanban. Bien que formé en full stack, c’est React qui me passionne le plus aujourd’hui, et je suis enthousiaste à l’idée de relever de nouveaux défis, d’apprendre constamment et de contribuer à des projets concrets et innovants."
+                        <>
+                            Ayant principalement travaillé en solo, j’ai appris à apprécier l’organisation et le suivi rigoureux des projets,
+                            notamment grâce aux méthodes agiles et à l’utilisation d’un tableau Kanban.<br></br>
+                            <br></br> Bien que formé en full stack, c’est React qui me passionne le plus aujourd’hui, et je suis enthousiaste à l’idée
+                            de relever de nouveaux défis, d’apprendre constamment et de contribuer à des projets concrets et innovants."
+                        </>
                     }
                 />
                 <AboutBlock
                     image={imgAmbitions}
                     title={"Projets et ambitions"}
                     text={
-                        "Mon rêve en tant que développeur est de créer une application pour les établissements scolaires qui motive les élèves via la gamification et une interface en pixel art, tout en facilitant l’organisation et le contact avec les parents. Pour y parvenir, je commence par travailler avec d’autres développeurs, réaliser des projets web pour de petites entreprises et aider les indépendants, afin d’acquérir expérience et contacts pour concrétiser mon objectif."
+                        <>
+                            Mon rêve en tant que développeur est de créer une application pour les établissements scolaires qui motive les élèves via
+                            la gamification et une interface en pixel art, tout en facilitant l’organisation et le contact avec les parents.<br></br>
+                            <br></br> Pour y parvenir, je commence par travailler avec d’autres développeurs, réaliser des projets web pour de petites
+                            entreprises et aider les indépendants, afin d’acquérir expérience et contacts pour concrétiser mon objectif.
+                        </>
                     }
                     reverse
                 />

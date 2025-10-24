d8bbf613ce4ddd2a51a88638d224cb1f28386c22
MrAbelDev <abelmruso@gmail.com>
2025-10-24 13:46:10 +0200

    animación hover project-card

--- src/assets/components/project-card/ProjectCard.jsx
+++ src/assets/components/project-card/ProjectCard.jsx
@@ -1,13 +1,18 @@
 import styles from "./ProjectCard.module.scss";
-import data from "../../data/data.json";
-import { FaSearch } from "react-icons/fa";
-import { useState } from "react";
 
-function ProjectCard({ mainImg, title, overlayColor, onOpenModal }) {
+function ProjectCard({ mainImg, title, description, overlayColor, onOpenModal }) {
     return (
         <article className={styles.card} style={{ "--overlay-color": overlayColor }}>
             <img src={mainImg} alt={title} />
-            <div className={styles.overlay}></div>
+            <div className={styles.overlay}>
+                <div className={styles.content}>
+                    <div className={styles.text}>
+                        <p>{description}</p>
+                    </div>
+
+                    <button onClick={onOpenModal}>Détails</button>
+                </div>
+            </div>
         </article>
     );
 }

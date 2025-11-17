29293577e7ef871f1d00e6640301cd15ab283f53
MrAbelDev <abelmruso@gmail.com>
2025-11-17 14:31:28 +0100

    rediseño general de las tarjetas de proyecto

--- src/assets/components/project-card/ProjectCard.jsx
+++ src/assets/components/project-card/ProjectCard.jsx
@@ -1,18 +1,18 @@
 import styles from "./ProjectCard.module.scss";
 import { useTranslation } from "react-i18next";
 
-function ProjectCard({ mainImg, title, description, overlayColor, onOpenModal }) {
+function ProjectCard({ mainImg, title, description, overlayColor, date, onOpenModal }) {
     const { t } = useTranslation();
     return (
-        <article className={styles.card} style={{ "--overlay-color": overlayColor }}>
+        <article onClick={onOpenModal} className={styles.card} style={{ "--overlay-color": overlayColor }}>
             <img src={mainImg} alt={title} />
-            <div className={styles.overlay}>
-                <div className={styles.content}>
-                    <div className={styles.text}>
-                        <p>{description}</p>
-                    </div>
-
-                    <button onClick={onOpenModal}>{t("projects.button")}</button>
+            <div className={styles.content}>
+                <div className={styles.text}>
+                    <p className={styles.title}>{title}</p>
+                    <p>{description}</p>
+                </div>
+                <div className={styles.bottomContent}>
+                    <p>{date}</p>
                 </div>
             </div>
         </article>
